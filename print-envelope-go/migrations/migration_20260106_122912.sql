e2b86a9dd15d851d1253b748b9e16f691ebeb9df
Adnan <mehedihasanadnan@gmail.com>
2026-01-06 14:03:03 +0600

    main changes

---
+++ print-envelope-go/migrations/migration_20260106_122912.sql
@@ -0,0 +1,15 @@
+-- Migration generated on 2026-01-06 12:29:12
+-- Total operations: 2
+
+-- [1] Drop unique constraint addresses.phone_no
+-- Type: drop_constraint
+-- Table: addresses
+-- Column: phone_no
+ALTER TABLE "addresses" DROP CONSTRAINT IF EXISTS "uq_addresses_phone_no";
+
+-- [2] Drop unique constraint returning_addresses.zip_code
+-- Type: drop_constraint
+-- Table: returning_addresses
+-- Column: zip_code
+ALTER TABLE "returning_addresses" DROP CONSTRAINT IF EXISTS "uq_returning_addresses_zip_code";
+
